42ba9c822df19eba720594687e4127c01630cc8e
Atoti2 <bogyikormi3@gmail.com>
2023-10-05 17:53:22 +0200

    aa

--- src/domManipulate.js
+++ src/domManipulate.js
@@ -319,14 +319,14 @@ function addProject() {
   });
 }
 
-// function renderProjects(project) {
-//   getLocalStorageProject();
-//   projectLi.innerHTML = "";
-//   project.forEach((project) => {
-//     projectLi.innerHTML += `
-//     <li class="calendar"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24"><title>menu</title><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>${project._title}</li>`;
-//   });
-// }
+function renderProjects(project) {
+  getLocalStorageProject();
+  projectLi.innerHTML = "";
+  project.forEach((project) => {
+    projectLi.innerHTML += `
+    <li class="calendar"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24"><title>menu</title><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>${project._title}</li>`;
+  });
+}
 
 
 function renderTodos(list) {
